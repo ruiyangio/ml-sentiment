9fb32f2459dbddbbb2b2e844073619a3497c3b15
ruiyangio <ruiyangwind@gmail.com>
2018-05-15 12:07:52 -0700

    Add svm version using sklearn

--- ml/validation.py
+++ ml/validation.py
@@ -1,6 +1,7 @@
 import os
 from bayes.skmnb import SkMultinomialNB
 from logistic.sklogistic import SkLogistic
+from svm.sksvm import SkSvm
 
 script_dir = os.path.dirname(__file__)
 train_set_path = script_dir + "/../resources/train_set.txt"
@@ -8,5 +9,7 @@ validation_set_path = script_dir + "/../resources/validation_set.txt"
 
 # model = SkLogistic(train_set_path, validation_set_path)
 # model.train_and_validate()
-nb = SkMultinomialNB(1, train_set_path, validation_set_path)
-nb.train_and_validate()
+# nb = SkMultinomialNB(1, train_set_path, validation_set_path)
+# nb.train_and_validate()
+svm = SkSvm(train_set_path, validation_set_path)
+svm.train_and_validate()
